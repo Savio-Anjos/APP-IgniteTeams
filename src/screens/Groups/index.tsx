515762cf04a75c339a5f0ea496266dbfd3f79b07
Savio Anjos <savioanjos2020@gmail.com>
2024-07-25 19:37:43 -0300

    feat: :sparkles: Added Import Alias

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,9 @@
-import { Container, Title } from "./styles";
+import * as S from "./styles";
 
 export function Groups() {
   return (
-    <Container>
-      <Title>Groups</Title>
-    </Container>
+    <S.Container>
+      <S.Title>Groups</S.Title>
+    </S.Container>
   );
 }
