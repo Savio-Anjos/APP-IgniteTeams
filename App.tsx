29994faf7719d95d740f59b05b6c9a628fce820a
Savio Anjos <savioanjos2020@gmail.com>
2024-07-30 15:36:08 -0300

    feat: :sparkles: Custom fonts configured

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups";
+
 import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
