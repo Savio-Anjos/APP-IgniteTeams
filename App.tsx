a382efd47028c2d4a80b7fee185b761ebb4d3c87
Savio Anjos <savioanjos2020@gmail.com>
2024-07-30 21:19:48 -0300

    feat: :sparkles: Added loading component

--- App.tsx
+++ App.tsx
@@ -7,14 +7,14 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { ActivityIndicator } from "react-native";
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {!fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
