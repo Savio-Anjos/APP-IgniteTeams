7c0bf119ff481cd9ffed62c13523c0bec8ac0f04
Savio Anjos <savioanjos2020@gmail.com>
2024-08-07 16:26:59 -0300

    type: :label: Updated GroupCard types

--- src/components/GroupCard/index.tsx
+++ src/components/GroupCard/index.tsx
@@ -5,9 +5,9 @@ type Props = TouchableOpacityProps & {
   title: string;
 };
 
-export function GroupCard({ title }: Props) {
+export function GroupCard({ title, ...rest }: Props) {
   return (
-    <Container>
+    <Container {...rest}>
       <Icon />
       <Title>{title}</Title>
     </Container>
