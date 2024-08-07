1c2edb5e6b6ce8875abfea791531ae79c5ed2981
Savio Anjos <savioanjos2020@gmail.com>
2024-08-07 15:06:24 -0300

    feat: :sparkles: Added Highlight component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -19,6 +19,6 @@ export const BackButton = styled.TouchableOpacity`
 `;
 
 export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
-  size: 36,
+  size: 32,
   color: theme.COLORS.WHITE,
 }))``;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,12 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 }
-("");
