a65ab28b75d245e09c98dfff51228d56eec5e615
Savio Anjos <savioanjos2020@gmail.com>
2024-08-07 14:49:18 -0300

    feat: :sparkles: Added icons library

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,19 @@
-import { Container, Logo } from "./styles";
+import React from "react";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 import logoImg from "@assets/logo.png";
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImg} />
     </Container>
   );
