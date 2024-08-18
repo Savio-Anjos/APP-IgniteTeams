260053651da2e21d8ded6538075fbbaf9180ceb2
Savio Anjos <savioanjos2020@gmail.com>
2024-08-18 15:13:50 -0300

    feat: :sparkles: Added ListEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,7 +6,7 @@ import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
