017cd2e8c6401a619152907032d50d5b1af60e07
Savio Anjos <savioanjos2020@gmail.com>
2024-08-19 11:59:51 -0300

    feat: :sparkles: Added Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,6 @@
+import { TextInputProps } from "react-native";
+import { Container } from "./styles";
+
+export function Input({ ...rest }: TextInputProps) {
+  return <Container {...rest} />;
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -13,8 +14,8 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="crie a turma para adicionar as pessoas"
         />
-
-        <Button title="Criar" />
+        <Input />
+        <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
