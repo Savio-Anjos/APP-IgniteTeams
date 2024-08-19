3e902e7d5e821874277bd3ac525cf8734880583c
Savio Anjos <savioanjos2020@gmail.com>
2024-08-19 11:36:53 -0300

    feat: :sparkles: Added NewPage screen

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import theme from "./src/theme";
 
 import { Groups } from "@screens/Groups";
 import { Loading } from "@components/Loading";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@components/Header";
+import { Container, Content, Icon } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { Button } from "@components/Button";
+
+export function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+      <Content>
+        <Icon />
+        <Highlight
+          title="Nova turma"
+          subtitle="crie a turma para adicionar as pessoas"
+        />
+
+        <Button title="Criar" />
+      </Content>
+    </Container>
+  );
+}
