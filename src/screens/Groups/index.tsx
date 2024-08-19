ee269436cafc0095c9fbf8d5589823c30707293f
Savio Anjos <savioanjos2020@gmail.com>
2024-08-19 10:36:21 -0300

    feat: :sparkles: Added Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,8 @@ import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -11,12 +13,19 @@ export function Groups() {
   return (
     <Container>
       <Header />
-      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-
-      <FlatList
-        data={groups}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+      <Highlight title="Turma" subtitle="jogue com a sua turma" />
+      {groups.length > 0 ? (
+        <FlatList
+          data={groups}
+          keyExtractor={(group) => group}
+          renderItem={({ item }) => <GroupCard title={item} />}
+        />
+      ) : (
+        <ListEmpty message="Nenhum grupo encontrado." />
+      )}
+      <Button
+        title="Adicionar novo grupo"
+        onPress={() => setGroups([...groups, "Novo grupo"])}
       />
     </Container>
   );
